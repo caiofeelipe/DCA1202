4a1e613a449389ca11efd0f0d18e6eca383149ad
Caio Felipe da Silva Alves <42450526+caiofeelipe@users.noreply.github.com>
2018-09-10 16:51:33 -0300

    Create questao8.c

---
+++ questao8.c
@@ -0,0 +1,4 @@
+p = mat + 1; -  Um ponteiro recebendo um endereço de memória do mesmo tipo dele
+
+x = (*mat)++; i.  Uma variável  
+inte irax recebendo o valor que está na primeira posição do vetor “mat”, que depois é incrementado
